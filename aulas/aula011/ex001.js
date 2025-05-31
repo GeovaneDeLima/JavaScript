e465c167592475a38ae2685a796e43d7a1fdc1f9
Geovane Lima <geovane020250@gmail.com>
2025-05-31 11:54:04 -0300

    primeira aula sobra condições

---
+++ aulas/aula011/ex001.js
@@ -0,0 +1,6 @@
+var vel 
+console.log('Sua velocidade atual é  '+ vel + 'km/h')
+
+if (vel >= 70){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
---
+++ aulas/aula011/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'EUS'
+
+if (pais == 'Brasil'){
+    console.log('Você é brasileiro ')
+}else{
+    console.log('Voce é estrangeiro')
+}
